c4dab41e497946e80c350b2d15e00226ef5656d2
valebuyondo <buyondovale@gmail.com>
2024-02-13 12:55:56 -0800

    0-m

--- 0x16-api_advanced/0-main.py
+++ 0x16-api_advanced/0-main.py
@@ -1,12 +1,16 @@
-#!/usr/bin/python3
-"""
-0-main
-"""
-import sys
+import requests
 
-if __name__ == '__main__':
-        number_of_subscribers = __import__('0-subs').number_of_subscribers
-            if len(sys.argv) < 2:
-                        print("Please pass an argument for the subreddit to search.")
-                            else:
-                                        print("{:d}".format(number_of_subscribers(sys.argv[1])))
+def number_of_subscribers(subreddit):
+    """Returns the number of subscribers for a given subreddit"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code != 200:
+        return 0
+    data = response.json().get('data')
+    if data:
+        return data.get('subscribers', 0)
+    else:
+        return 0
+
