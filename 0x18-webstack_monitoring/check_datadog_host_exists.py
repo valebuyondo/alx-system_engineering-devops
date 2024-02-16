cb253b01afd138dd3d540d0cebe4368f91c1c84e
Buyondo Vale <42058151+valebuyondo@users.noreply.github.com>
2024-02-16 04:34:17 +0300

    Update check_datadog_host_exists.py

--- 0x18-webstack_monitoring/check_datadog_host_exists.py
+++ 0x18-webstack_monitoring/check_datadog_host_exists.py
@@ -13,26 +13,25 @@ DATADOG_APP_KEY = "ee6ea7fd92edfccb4cc74f953ebf53e0e5b6d4a4"
 HOSTNAME = "422436-web-01"
 
 def check_host_existence(hostname):
-        url = f"{DATADOG_API_URL}hosts/{hostname}"
-            headers = {
-                            "Content-Type": "application/json",
-                                    "DD-API-KEY": DATADOG_API_KEY,
-                                            "DD-APPLICATION-KEY": DATADOG_APP_KEY
-                                                }
-                response = requests.get(url, headers=headers)
-                    if response.status_code == 200:
-                                return True
-                                elif response.status_code == 404:
-                                            return False
-                                            else:
-                                                        print(f"Failed to check host existence. Status code: {response.status_code}")
-                                                                return None
+    url = f"{DATADOG_API_URL}hosts/{hostname}"
+    headers = {
+        "Content-Type": "application/json",
+        "DD-API-KEY": DATADOG_API_KEY,
+        "DD-APPLICATION-KEY": DATADOG_APP_KEY
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return True
+    elif response.status_code == 404:
+        return False
+    else:
+        print(f"Failed to check host existence. Status code: {response.status_code}")
+        return None
 
-                                                            if __name__ == "__main__":
-                                                                    exists = check_host_existence(HOSTNAME)
-                                                                        if exists is not None:
-                                                                                    if exists:
-                                                                                                    print(f"The host '{HOSTNAME}' exists in DataDog.")
-                                                                                                            else:
-                                                                                                                            print(f"The host '{HOSTNAME}' does not exist in DataDog.")
-
+if __name__ == "__main__":
+    exists = check_host_existence(HOSTNAME)
+    if exists is not None:
+        if exists:
+            print(f"The host '{HOSTNAME}' exists in DataDog.")
+        else:
+            print(f"The host '{HOSTNAME}' does not exist in DataDog.")
