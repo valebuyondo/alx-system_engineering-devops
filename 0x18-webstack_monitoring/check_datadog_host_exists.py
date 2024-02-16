2b37f88fa3058f6bc15b3a362a7c9deae3cd0f83
valebuyondo <buyondovale@gmail.com>
2024-02-15 17:50:44 -0800

    check_datadog_host_exists.py

--- 0x18-webstack_monitoring/check_datadog_host_exists.py
+++ 0x18-webstack_monitoring/check_datadog_host_exists.py
@@ -1,37 +1,16 @@
-import requests
+"""
+Get all hosts for your organization returns "OK" response
+"""
 
-# DataDog API base URL
-DATADOG_API_URL = "https://api.datadoghq.com/api/v1/"
+from datadog_api_client import ApiClient, Configuration
+from datadog_api_client.v1.api.hosts_api import HostsApi
 
-# Your DataDog API key
-DATADOG_API_KEY = "04f178090000fa3a355d3d925ad673ca"
+configuration = Configuration()
+with ApiClient(configuration) as api_client:
+        api_instance = HostsApi(api_client)
+            response = api_instance.list_hosts(
+                            filter="env:ci",
+                                )
 
-# Your DataDog application key
-DATADOG_APP_KEY = "925d87a468140f04ff21539639823d90102d216e"
+                print(response)
 
-# Hostname to check
-HOSTNAME = "422436-web-01"
-
-def check_host_existence(hostname):
-    url = f"{DATADOG_API_URL}hosts/{hostname}"
-    headers = {
-        "Content-Type": "application/json",
-        "DD-API-KEY": DATADOG_API_KEY,
-        "DD-APPLICATION-KEY": DATADOG_APP_KEY
-    }
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        return True
-    elif response.status_code == 404:
-        return False
-    else:
-        print(f"Failed to check host existence. Status code: {response.status_code}")
-        return None
-
-if __name__ == "__main__":
-    exists = check_host_existence(HOSTNAME)
-    if exists is not None:
-        if exists:
-            print(f"The host '{HOSTNAME}' exists in DataDog.")
-        else:
-            print(f"The host '{HOSTNAME}' does not exist in DataDog.")
