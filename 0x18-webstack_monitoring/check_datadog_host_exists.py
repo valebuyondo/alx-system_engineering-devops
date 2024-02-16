d6a0d3698ab3adf7d9d351d4b0062c2319abfb92
Buyondo Vale <42058151+valebuyondo@users.noreply.github.com>
2024-02-16 04:51:28 +0300

    Update check_datadog_host_exists.py

--- 0x18-webstack_monitoring/check_datadog_host_exists.py
+++ 0x18-webstack_monitoring/check_datadog_host_exists.py
@@ -7,10 +7,9 @@ from datadog_api_client.v1.api.hosts_api import HostsApi
 
 configuration = Configuration()
 with ApiClient(configuration) as api_client:
-        api_instance = HostsApi(api_client)
-            response = api_instance.list_hosts(
-                            filter="env:ci",
-                                )
+    api_instance = HostsApi(api_client)
+    response = api_instance.list_hosts(
+        filter="env:ci",
+    )
 
-                print(response)
-
+    print(response)
